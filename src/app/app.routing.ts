803e6bbbffd1286b7a009c3242dc67201d51eaf8
Tuag <igortuag@gmail.com>
2022-01-31 06:28:22 -0300

    add app routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ChampionDetailComponent } from './champion-detail/champion-detail.component';
+import { ChampionListComponent } from './champion-list/champion-list.component';
+
+const appRoutes = [
+  { path: '', component: ChampionListComponent },
+  { path: '/:id', component: ChampionDetailComponent }
+]
+
+export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes);
