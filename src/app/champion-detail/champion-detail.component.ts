0e33cb5e5cd56bc1713743bb601d53cd52746f51
Tuag <igortuag@gmail.com>
2022-01-31 06:46:14 -0300

    show champion details

--- src/app/champion-detail/champion-detail.component.ts
+++ src/app/champion-detail/champion-detail.component.ts
@@ -1,4 +1,8 @@
+import { ChampionService } from './../champion-list/champion.service';
 import { Component, OnInit } from '@angular/core';
+import { Champion } from '../champion-list/champion';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-champion-detail',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChampionDetailComponent implements OnInit {
 
-  constructor() { }
+  subscription: Subscription
+
+  champion: Champion;
+
+  constructor(
+    private ChampionService: ChampionService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.ChampionService.getChampion(id).then(champion => this.champion = champion);
+    })
   }
 
 }
